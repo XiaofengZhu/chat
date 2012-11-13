3022dc2b88cc563ee5f516b8f5d794a2d2958b13
Chris Sonnenberg <damikin11@gmail.com>
2012-11-12 22:38:06 -0800

    Multiple changes!
    Added a struct that is kinda like a packet...it holds information and passes it to the main thingy (login, messages, logout).  This should stop any crashing that may have happened from the previous version.
    
    made everything a function instead of inline functions.
    
    dropped to 3 well defined channels.
    - done (which will kill the server after a signal is received)
    - sigs (which waits for specified signals)
    - packet (which handles all packets from connections (login, messages, logout)

--- server.go
+++ server.go
@@ -11,9 +11,93 @@ import (
 	"time"
 )
 
-// connections are handled here
-func connections(incomingtext chan string) {
-	// setup the protocol and port to listen on
+type packet struct {
+	Pname       string
+	Pconnection net.Conn
+	Ptime       time.Time
+	Pmessage    string
+	Ptype       int
+}
+
+const TYPE_LOGIN int = 0
+const TYPE_MESSAGE int = 1
+const TYPE_LOGOUT int = 2
+
+func handlemessage(message string, connections map[string]packet) {
+	for _, user := range connections {
+		output := bufio.NewWriter(user.Pconnection)
+		output.WriteString(message)
+		output.Flush()
+	}
+}
+
+func handleconnection(c net.Conn, packetchan chan packet) {
+	var user packet
+	input := bufio.NewReader(c)
+	output := bufio.NewWriter(c)
+	output.WriteString("What is your name? ")
+	output.Flush()
+	name, _ := input.ReadString('\n')
+
+	user.Pname = strings.TrimSpace(name)
+	user.Pconnection = c
+	user.Ptime = time.Now()
+	user.Ptype = TYPE_LOGIN
+	packetchan <- user
+
+	for {
+		line, err := input.ReadString('\n')
+		if err != nil {
+			break
+		} else if len(line) > 2 {
+			user.Pmessage = user.Pname + "> " + line
+			user.Ptype = TYPE_MESSAGE
+			user.Ptime = time.Now()
+			packetchan <- user
+		}
+	}
+
+	user.Ptype = TYPE_LOGOUT
+	user.Ptime = time.Now()
+	packetchan <- user
+
+	user.Pconnection.Close()
+}
+
+func handlepacket(packetchan chan packet) {
+	connections := make(map[string]packet)
+	for {
+		packetrec := <- packetchan
+		switch packetrec.Ptype {
+		case TYPE_LOGIN:
+			connections[packetrec.Pname] = packetrec
+
+		case TYPE_MESSAGE:
+			go handlemessage(packetrec.Pmessage, connections)
+
+		case TYPE_LOGOUT:
+			delete(connections, packetrec.Pname)
+
+		default:
+			// nothing
+		}
+	}
+}
+
+func accept(packetchan chan packet, listen net.Listener) {
+	for {
+		connection, err := listen.Accept()
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("Exiting...")
+			os.Exit(2)
+		}
+
+		go handleconnection(connection, packetchan)
+	}
+}
+
+func start(packetchan chan packet) {
 	listen, err := net.Listen("tcp", ":2000")
 	if err != nil {
 		fmt.Println(err)
@@ -21,100 +105,37 @@ func connections(incomingtext chan string) {
 		os.Exit(1)
 	}
 
-	incomingconn := make(chan *bufio.Writer, 100)
-
-	// this go routine handles all connections and chatting!
-	// i could not get lists to work for some reason, so i went
-	//  with a slice for connections (cannot remove connections so
-	//  will probably die badly when someone leaves early)
-	//  and all text is sent to every current connection when received
-	//  (again, through it's own go routine)
-	go func() {
-		connslice := make([]*bufio.Writer, 0)
-		for {
-			select {
-			case newconnect := <-incomingconn:
-				connslice = append(connslice, newconnect)
-			case newtext := <-incomingtext:
-				go func(cs []*bufio.Writer, nt string) {
-					for _, v := range cs {
-						v.WriteString(nt)
-						v.Flush()
-					}
-				}(connslice, newtext)
-			}
-		}
-	}()
-
-	// start accepting connections in a go routine so we can
-	//  handle signals without being blocked
-	go func() {
-		for {
-			connection, err := listen.Accept()
-			if err != nil {
-				fmt.Println(err)
-				fmt.Println("Exiting...")
-				os.Exit(2)
-			}
-
-			// handling each connection in a go routine
-			//  so we can accept more connections
-			go func(c net.Conn) {
-				input := bufio.NewReader(c)
-				output := bufio.NewWriter(c)
-				output.WriteString("What is your name? ")
-				output.Flush()
-				name, _ := input.ReadString('\n')
-				name = strings.TrimSpace(name)
-				incomingconn <- output
-
-				for {
-					line, err := input.ReadString('\n')
-					if err != nil {
-						break
-					} else if len(line) > 2 {
-						incomingtext <- name + "> " + line
-					}
-				}
-
-				c.Close()
-			}(connection)
-		}
-	}()
+	go handlepacket(packetchan)
+	go accept(packetchan, listen)
 }
 
 // signal handling (and sending true through done)
-func signals(done chan bool, it chan string) {
-	sigs := make(chan os.Signal, 1)
+func signals(donechan chan bool, packetchan chan packet) {
+	sigschan := make(chan os.Signal, 1)
+	signal.Notify(sigschan, syscall.SIGINT, syscall.SIGTERM)
 
-	// these are the signals we will respawn to with "sigs" channel
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	var serveruser packet
+	serveruser.Pname = "Server"
+	serveruser.Ptime = time.Now()
+	serveruser.Pmessage = serveruser.Pname + "> Server going down in 10 seconds!\n"
+	serveruser.Ptype = TYPE_MESSAGE
 
-	// start a go routine so we don't stop execution
-	go func() {
-		<-sigs // blocking until we get a signal
-		it <- "!! Server> Server going down in 10 seconds!"
-		time.Sleep(time.Second * 10)
-		done <- true // tell that we are done because of signals
-	}()
+	<- sigschan
+	packetchan <- serveruser
+	time.Sleep(time.Second * 10)
+	donechan <- true
 }
 
 // pulls everything together
 func main() {
-	// done channel for signals, and incoming text (so we can send
-	//  server information because of shutdown)
-	done := make(chan bool, 1)
-	incomingtext := make(chan string, 100)
+	donechan := make(chan bool, 1)
+	packetchan := make(chan packet, 100)
 
-	// setup signal handling then accept connections
-	signals(done, incomingtext)
-	connections(incomingtext)
+	go signals(donechan, packetchan)
+	go start(packetchan)
 
-	// we are fully up and running now!
 	fmt.Println("Server running now!")
-
-	<-done
-
+	<-donechan
 	fmt.Println()
 	fmt.Println("Server exiting now!")
 }
